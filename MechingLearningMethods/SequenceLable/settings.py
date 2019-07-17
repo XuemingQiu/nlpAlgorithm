a98e694b26122c464cb1e34de9a23070d249f2ea
xuemingqiu <1806060082@qq.com>
2019-07-17 21:53:02 +0800

    增加lstm模型构建的代码路径

--- MechingLearningMethods/SequenceLable/settings.py
+++ MechingLearningMethods/SequenceLable/settings.py
@@ -16,5 +16,15 @@ train_path = os.path.join(path, "testDatas/Sequencelable_train.txt")
 train_data_path = os.path.join(path, "testDatas/train_data.txt")
 train_lable_path = os.path.join(path, "testDatas/train_lable.txt")
 
-# 模型参数
+# 预训练的词向量的路径
+# 这里是利用gensim进行训练的，通过numpy进行保存的，可以换成其他预训练的，但是在读取的时候需要改动。
+word_embeding_path = os.path.join(path, "model/word_embedding.npy")
+
+# 双向的lstm模型的参数设置
+input_size = 300  # 也是word_embedding词向量的维度
+hidden_size = 128  # 隐藏层的单元个数
+num_layers = 5  # 有5层隐藏层
+num_epochs = 5  # 训练迭代轮数
+learning_rate = 0.001  # 学习率
+# 模型的输出路径
 output_model_path = os.path.join(path, "model/lstmmodel")
